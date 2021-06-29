b88a627061018c0aeafc86599a912a613ed38b3d
Mahika9265 <72756971+Mahika9265@users.noreply.github.com>
2021-06-29 11:53:57 -0500

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,13 @@
+class box{}
+    var options={
+        isStatic=false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+    var side1;
+    var side2;
+    var side3;
+    side1=createSprite(250,20,100,20);
+    side2=createSprite(width/2,20,20,200);
+    side3=createSprite(450,20,100,20);
